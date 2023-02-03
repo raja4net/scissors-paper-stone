dea79420fcc813a1927c31bc0551060f75398c31
Raja Rupinder Singh <raja4net@yahoo.com>
2023-02-03 21:42:47 +0530

    Completed UI & game logic

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ let computerScore = 0;
 let gameEnds = false;
 
 function playRound(playerSelection, computerSelection) {
+  console.log(userImage.getAttribute("src"));
+
+  userImage.style.display = "block";
+  computerImage.style.display = "block";
+  userImage.setAttribute("src", `img/${playerSelection}.jpeg`);
+  computerImage.setAttribute("src", `img/${computerSelection}.jpeg`);
+
   console.log("Player:", playerSelection);
   console.log("Computer", computerSelection);
   if (playerSelection === computerSelection) {
@@ -23,26 +30,43 @@ function playRound(playerSelection, computerSelection) {
     playerScore += 1;
   }
   round++;
+  document.getElementById("player").innerHTML = `You: ${playerScore}`;
+  document.getElementById("computer").innerHTML = `Computer: ${computerScore}`;
   checkGame();
 }
 
+function resetGame() {
+  round = 0;
+  playerScore = 0;
+  computerScore = 0;
+  gameEnds = false;
+  userImage.style.display = "none";
+  computerImage.style.display = "none";
+  document.getElementById("player").innerHTML = `You: ${playerScore}`;
+  document.getElementById("computer").innerHTML = `Computer: ${computerScore}`;
+  verdict.innerHTML = "";
+  playAgain.style.display = "none";
+}
+
 function getWinner() {
+  const verdict = document.getElementById("verdict");
+
   if (playerScore > computerScore) {
-    document.getElementById("verdict").innerHTML = "Congrats! You won";
+    verdict.innerHTML = "Congrats! You won";
   } else {
-    document.getElementById("verdict").innerHTML = "Sorry! You lose";
+    verdict.innerHTML = "Sorry! You lose";
   }
+
+  playAgain.style.display = "block";
 }
 
 function checkGame() {
-  if (playerScore < 5 && computerScore < 5) {
-    gameEnds = false;
-  } else {
+  if (playerScore == 5 || computerScore == 5) {
     gameEnds = true;
     getWinner();
+  } else {
+    gameEnds = false;
   }
-  document.getElementById("player").innerHTML = playerScore;
-  document.getElementById("computer").innerHTML = computerScore;
 }
 
 function getComputerChoice() {
@@ -50,15 +74,10 @@ function getComputerChoice() {
   return choices[choice];
 }
 
-function btnClicked(e) {
-  console.log("button clicked");
-  if (e.target.id === "rock") {
-    playerSelection = "rock";
-  } else if (e.target.id === "paper") {
-    playerSelection = "paper";
-  } else {
-    playerSelection = "scissors";
-  }
+function btnClicked() {
+  // console.log(e.target.id);
+  // playerSelection = String(e.target.id);
+
   console.log(playerSelection);
   computerSelection = getComputerChoice();
   if (gameEnds == false) {
@@ -69,12 +88,21 @@ function btnClicked(e) {
 const btnChoice = document.querySelectorAll("button");
 console.log(btnChoice);
 btnChoice.forEach((btn) => {
-  btn.addEventListener("click", btnClicked);
+  btn.addEventListener("click", function () {
+    playerSelection = `${btn.id}`;
+    btnClicked();
+  });
 });
 
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const computerImage = document.getElementById("computerImage");
+const userImage = document.getElementById("userImage");
+const selectedImages = document.querySelectorAll(".selection");
+const playAgain = document.querySelector(".play-again");
 
-document.getElementById("player").innerHTML = playerScore;
-document.getElementById("computer").innerHTML = computerScore;
+document.getElementById("player").innerHTML = `You: ${playerScore}`;
+document.getElementById("computer").innerHTML = `Computer: ${computerScore}`;
+
+document.getElementById("play-again").addEventListener("click", resetGame);
